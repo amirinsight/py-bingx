247a5de6c715a67325c27fb688b038cd5e28e4c4
Amirhossein Zare <77607745+amirinsight@users.noreply.github.com>
2023-09-15 16:59:58 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,33 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='py-bingx',
-    version='0.1',
+    version='0.2',
     license='MIT',
-    author="M1raX",
-    author_email='mirax@mirax.com',
-    packages=find_packages('bingx'),
-    package_dir={'': 'bingx'},
-    url='https://github.com/M1raX/py-bingx',
-    keywords='bingx api',
+    author="Amirhossein Zare",
+    description='BingX REST API Python implementation',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author_email='amiryzare@gmail.com',
+    packages=['bingx'],
+    url='https://github.com/amirinsight/py-bingx',
+    keywords='bingx api cryptocurrency trading btc eth rest exchange',
     install_requires=[
         'requests',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 
 )
