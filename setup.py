71bef13d31aa0b53ba60f8495ab7f739534b7d01
amirinsight <77607745+amirinsight@users.noreply.github.com>
2023-10-01 02:35:17 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name='py-bingx',
-    version='0.2',
+    version='0.4',
     license='MIT',
     author="Amirhossein Zare",
     description='BingX REST API Python implementation',
