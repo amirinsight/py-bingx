063669730cb90eb64565546398e1707d07d36660
amirinsight <77607745+amirinsight@users.noreply.github.com>
2023-10-20 18:17:43 +0330

    Update exceptions.py

--- bingx/exceptions.py
+++ bingx/exceptions.py
@@ -2,7 +2,8 @@
 # {'code': 80014, 'msg': 'signature not match', 'data': {}}
 # {'code': 80014, 'msg': 'Insufficient margin, please adjust and resubmit', 'data': {}} # TODO: return mgs if code is not 0.
 # {'code': 80012, 'msg': 'the account has positions or pending orders', 'data': {}}
-
+# {'code': 80014, 'msg': 'Take Profit price should be lower than trigger price', 'data': {}} in Short trades
+# {'code': 80014, 'msg': 'Insufficient position, please adjust and resubmit', 'data': {}} place_market_order("ALGO-USDT", "Short", 5, "Close")
 # {'code': 80014, 'msg': 'Invalid parameters, err:strconv.ParseFloat: parsing "None": invalid syntax', 'data': {}}
 """def place_market_order(self, pair, position_side, volume, action_of_choice, tp=" ", sl=" "):
     path = "/api/v1/user/trade"
@@ -25,5 +26,3 @@
     # data = response["data"]
     return response
 """
-# {'code': 80014, 'msg': 'Take Profit price should be lower than trigger price', 'data': {}} in Short trades
-# {'code': 80014, 'msg': 'Insufficient position, please adjust and resubmit', 'data': {}} place_market_order("ALGO-USDT", "Short", 5, "Close")
