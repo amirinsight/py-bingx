450069daa065eca4214653b160a6eacd6595cdee
franciszek szeptycki <franciszek.szeptycki@gmail.com>
2023-10-20 22:16:25 +0200

    random user agent

--- bingx/utilities.py
+++ bingx/utilities.py
@@ -1,4 +1,6 @@
 import time
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
 
 
 def convert_date_to_epoch_ms(time):
@@ -7,3 +9,12 @@ def convert_date_to_epoch_ms(time):
 
 def get_system_time():
     return str(int(time.time() * 1000))
+
+def get_random_agent():
+    software_names = [SoftwareName.CHROME.value, SoftwareName.SAFARI.value, SoftwareName.FIREFOX.value, SoftwareName.OPERA.value, SoftwareName.EDGE.value]
+    operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.MAC.value, OperatingSystem.ANDROID.value, OperatingSystem.IOS.value]   
+    user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+    return user_agent_rotator.get_random_user_agent()
+
+
+print(get_random_agent())
