689c64dddff84dd5d34cde1bf4d5b7c8bcd81026
Danial <danial.rikhtehgaran@gmail.com>
2024-03-03 19:19:45 +0330

    Test BingX

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name='py-bingx',
-    version='0.4',
+    version='0.5',
     license='MIT',
     author="Amirhossein Zare",
     description='BingX REST API Python implementation',
